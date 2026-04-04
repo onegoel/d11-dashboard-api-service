2232788e7ee898522e671d7631663eb613744ffe
Aryan Goel <aryangoel6900@yahoo.in>
2026-04-05 05:15:43 +0530

    chore: files cleanup

--- prisma/seeders/matches.seed.d.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaClient } from "../../generated/prisma/client.js";
-export declare function seedFixtures(prisma: PrismaClient, seasonId: number): Promise<void>;
-//# sourceMappingURL=matches.seed.d.ts.map
--- prisma/seeders/seasonUsers.seed.d.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaClient } from "../../generated/prisma/client.js";
-export declare function seedSeasonUsers(prisma: PrismaClient, seasonId: number): Promise<void>;
-//# sourceMappingURL=seasonUsers.seed.d.ts.map
